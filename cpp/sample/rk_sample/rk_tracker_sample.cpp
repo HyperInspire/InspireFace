2dff5ef43576f62c4200c7a7e8c7b9e4e9b7ef3f
JingyuYan <tunmxy@163.com>
2024-02-29 00:07:18 +0800

    fix cmake

--- cpp/sample/rk_sample/rk_tracker_sample.cpp
+++ cpp/sample/rk_sample/rk_tracker_sample.cpp
@@ -5,7 +5,7 @@
 #include "opencv2/opencv.hpp"
 #include "inspireface/middleware/model_loader/ModelLoader.h"
 #include "model_index.h"
-#include "inspireface/middleware/Timer.h"
+#include "inspireface/middleware/timer.h"
 #include "inspireface/FaceContext.h"
 
 using namespace hyper;
