9c04ed5e4e348f926b8b339c697efa32c26ad883
JingyuYan <tunmxy@163.com>
2024-03-20 19:47:12 +0800

    add test full
    Former-commit-id: 264e13511d4633ee189da77a11928d4f6c54c9a1

--- cpp/test/unit/source/test_source_face_face_data.cpp
+++ cpp/test/unit/source/test_source_face_face_data.cpp
@@ -2,7 +2,7 @@
 // Created by tunm on 2023/9/17.
 //
 
-#include "common/test_settings.h"
+#include "settings/test_settings.h"
 #include "inspireface/common/face_data/data_tools.h"
 #include "herror.h"
 #include "inspireface/face_context.h"
