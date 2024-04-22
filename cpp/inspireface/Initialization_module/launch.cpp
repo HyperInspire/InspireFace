3158d14d2b048c778a82c1efd473345eb40f7c6b
JingyuYan <tunmxy@163.com>
2024-04-22 15:49:18 +0800

    fix api

--- cpp/inspireface/Initialization_module/launch.cpp
+++ cpp/inspireface/Initialization_module/launch.cpp
@@ -2,29 +2,29 @@
 // Created by tunm on 2024/4/17.
 //
 
-#include "model_hub.h"
+#include "launch.h"
 #include "log.h"
 #include "herror.h"
 
 
 namespace inspire {
 
-std::mutex ModelHub::mutex_;
-std::shared_ptr<ModelHub> ModelHub::instance_ = nullptr;
+std::mutex Launch::mutex_;
+std::shared_ptr<Launch> Launch::instance_ = nullptr;
 
-InspireArchive& ModelHub::getMArchive() {
+InspireArchive& Launch::getMArchive() {
     return m_archive_;
 }
 
-std::shared_ptr<ModelHub> ModelHub::GetInstance() {
+std::shared_ptr<Launch> Launch::GetInstance() {
     std::lock_guard<std::mutex> lock(mutex_);
     if (!instance_) {
-        instance_ = std::shared_ptr<ModelHub>(new ModelHub());
+        instance_ = std::shared_ptr<Launch>(new Launch());
     }
     return instance_;
 }
 
-int32_t ModelHub::Load(const std::string &path) {
+int32_t Launch::Load(const std::string &path) {
     if (!m_load_) {
         m_archive_.ReLoad(path);
         if (m_archive_.QueryStatus() == SARC_SUCCESS) {
@@ -39,7 +39,7 @@ int32_t ModelHub::Load(const std::string &path) {
     }
 }
 
-bool ModelHub::isMLoad() const {
+bool Launch::isMLoad() const {
     return m_load_;
 }
 
