c1aa4084285f09d852d5e812e4846c0501fb68e9
JingyuYan <tunmxy@163.com>
2024-04-30 15:14:08 +0800

    Adjust directory structure
    Former-commit-id: f600ec703052fc96f694a9ca284ddd5171158a23

--- command/build_linux_cuda.sh
+++ command/build_linux_cuda.sh
@@ -3,13 +3,14 @@ mkdir -p build/linux_cuda
 cd build/linux_cuda
 # export cross_compile_toolchain=/home/s4129/software/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf
 cmake -DCMAKE_SYSTEM_NAME=Linux \
+  -DCMAKE_BUILD_TYPE=Release \
   -DBUILD_WITH_SAMPLE=ON \
   -DBUILD_WITH_TEST=ON \
   -DENABLE_BENCHMARK=ON \
   -DENABLE_USE_LFW_DATA=ON \
   -DENABLE_TEST_EVALUATION=ON \
   -DGLOBAL_INFERENCE_BACKEND_USE_MNN_CUDA=ON \
-  -DLINUX_MNN_CUDA=/home/s4027/software/MNN-2.7.0/build_cuda/install \
+  -DLINUX_MNN_CUDA=/home/tunm/software/MNN-2.7.0/build_cuda/install \
   -DBUILD_SHARED_LIBS=ON ../..
 
 make -j4
