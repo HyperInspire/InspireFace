95818938ae0e59096fe30fec8b9499820cfccfaf
JingyuYan <tunmxy@163.com>
2024-04-30 17:27:46 +0800

    readme

--- command/build.sh
+++ command/build.sh
@@ -3,11 +3,12 @@ mkdir -p build/local
 cd build/local
 # export cross_compile_toolchain=/home/s4129/software/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf
 cmake -DCMAKE_BUILD_TYPE=Release \
+  -DTHIRD_PARTY_DIR=3rdparty \
   -DBUILD_WITH_SAMPLE=ON \
-  -DBUILD_WITH_TEST=ON \
-  -DENABLE_BENCHMARK=ON \
-  -DENABLE_USE_LFW_DATA=ON \
-  -DENABLE_TEST_EVALUATION=ON \
+  -DBUILD_WITH_TEST=OFF \
+  -DENABLE_BENCHMARK=OFF \
+  -DENABLE_USE_LFW_DATA=OFF \
+  -DENABLE_TEST_EVALUATION=OFF \
   -DBUILD_SHARED_LIBS=ON ../../
 
 make -j4
--- command/build_linux_cuda.sh
+++ command/build_linux_cuda.sh
@@ -4,11 +4,12 @@ cd build/linux_cuda
 # export cross_compile_toolchain=/home/s4129/software/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf
 cmake -DCMAKE_SYSTEM_NAME=Linux \
   -DCMAKE_BUILD_TYPE=Release \
-  -DBUILD_WITH_SAMPLE=ON \
-  -DBUILD_WITH_TEST=ON \
-  -DENABLE_BENCHMARK=ON \
-  -DENABLE_USE_LFW_DATA=ON \
-  -DENABLE_TEST_EVALUATION=ON \
+  -DTHIRD_PARTY_DIR=3rdparty \
+  -DBUILD_WITH_SAMPLE=OFF \
+  -DBUILD_WITH_TEST=OFF \
+  -DENABLE_BENCHMARK=OFF \
+  -DENABLE_USE_LFW_DATA=OFF \
+  -DENABLE_TEST_EVALUATION=OFF \
   -DGLOBAL_INFERENCE_BACKEND_USE_MNN_CUDA=ON \
   -DLINUX_MNN_CUDA=/home/tunm/software/MNN-2.7.0/build_cuda/install \
   -DBUILD_SHARED_LIBS=ON ../..
