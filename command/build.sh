8078bbcc89abcd5dc714ee4bae2f9bc2d3065217
JingyuYan <tunmxy@163.com>
2024-05-04 23:50:06 +0800

    Added OpenCV from submodule

--- command/build.sh
+++ command/build.sh
@@ -3,7 +3,6 @@ mkdir -p build/local
 cd build/local
 # export cross_compile_toolchain=/home/s4129/software/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf
 cmake -DCMAKE_BUILD_TYPE=Release \
-  -DTHIRD_PARTY_DIR=3rdparty \
   -DBUILD_WITH_SAMPLE=ON \
   -DBUILD_WITH_TEST=OFF \
   -DENABLE_BENCHMARK=OFF \
