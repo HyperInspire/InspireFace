e683dc58c18f6e3ed65e66fbffde5b1ed4358178
JingyuYan <tunmxy@163.com>
2024-05-05 03:33:42 +0800

    Added online submodules
    Former-commit-id: 737d1705bcffca20aaf2a5c21bbfa208b8315ea6

--- command/build.sh
+++ command/build.sh
@@ -3,12 +3,12 @@ mkdir -p build/local
 cd build/local
 # export cross_compile_toolchain=/home/s4129/software/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf
 cmake -DCMAKE_BUILD_TYPE=Release \
-  -DBUILD_WITH_SAMPLE=ON \
-  -DBUILD_WITH_TEST=OFF \
-  -DENABLE_BENCHMARK=OFF \
-  -DENABLE_USE_LFW_DATA=OFF \
-  -DENABLE_TEST_EVALUATION=OFF \
-  -DBUILD_SHARED_LIBS=ON ../../
+  -DISF_BUILD_WITH_SAMPLE=ON \
+  -DISF_BUILD_WITH_TEST=OFF \
+  -DISF_ENABLE_BENCHMARK=OFF \
+  -DISF_ENABLE_USE_LFW_DATA=OFF \
+  -DISF_ENABLE_TEST_EVALUATION=OFF \
+  -DISF_BUILD_SHARED_LIBS=ON ../../
 
 make -j4
 make install
--- command/build_linux_cuda.sh
+++ command/build_linux_cuda.sh
@@ -4,13 +4,13 @@ cd build/linux_cuda
 # export cross_compile_toolchain=/home/s4129/software/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf
 cmake -DCMAKE_SYSTEM_NAME=Linux \
   -DCMAKE_BUILD_TYPE=Release \
-  -DBUILD_WITH_SAMPLE=OFF \
-  -DBUILD_WITH_TEST=OFF \
-  -DENABLE_BENCHMARK=OFF \
-  -DENABLE_USE_LFW_DATA=OFF \
-  -DENABLE_TEST_EVALUATION=OFF \
-  -DGLOBAL_INFERENCE_BACKEND_USE_MNN_CUDA=ON \
-  -DLINUX_MNN_CUDA=/home/tunm/software/MNN-2.7.0/build_cuda/install \
-  -DBUILD_SHARED_LIBS=ON ../..
+  -DISF_BUILD_WITH_SAMPLE=OFF \
+  -DISF_BUILD_WITH_TEST=OFF \
+  -DISF_ENABLE_BENCHMARK=OFF \
+  -DISF_ENABLE_USE_LFW_DATA=OFF \
+  -DISF_ENABLE_TEST_EVALUATION=OFF \
+  -DISF_GLOBAL_INFERENCE_BACKEND_USE_MNN_CUDA=ON \
+  -DISF_LINUX_MNN_CUDA=/home/tunm/software/MNN-2.7.0/build_cuda/install \
+  -DISF_BUILD_SHARED_LIBS=ON ../..
 
 make -j4
