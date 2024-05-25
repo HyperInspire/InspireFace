16af5bb5c2b574b90c05a7b2415d0d9131fe2730
JingyuYan <tunmxy@163.com>
2024-05-26 03:55:44 +0800

    call
    Former-commit-id: 7e75857fdb7d74f588df3af3739312b4fa077f06

--- cpp/sample/cpp/sample_face_track_video.cpp
+++ cpp/sample/cpp/sample_face_track_video.cpp
@@ -37,7 +37,7 @@ int main(int argc, char* argv[]) {
         return ret;
     }
 
-    HFSessionSetTrackPreviewSize(session, 640);
+//    HFSessionSetTrackPreviewSize(session, 640);
 
     // Open the video file
     cv::VideoCapture cap(videoPath);
