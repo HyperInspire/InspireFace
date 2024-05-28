d47fba0ee20180c0ee9b2029b5569cdc2288e3f7
JingyuYan <tunmxy@163.com>
2024-05-29 00:32:51 +0800

    add omi

---
+++ cpp/inspireface/middleware/omni_inference/omi_inference.h
@@ -0,0 +1,20 @@
+#ifndef INSPIREFACE_OMNI_INFERENACE__
+#define INSPIREFACE_OMNI_INFERENACE__
+
+enum {
+    anyDataTypeVoid,
+    anyDataTypeUint8,
+    anyDataTypeInt8,
+    anyDataTypeFp32,
+    anyDataTypeInt32,
+    anyDataTypeInt64,
+} AnyDataType;
+
+class InputAdapter {
+public:
+
+
+
+};
+
+#endif  // INSPIREFACE_OMNI_INFERENACE__
