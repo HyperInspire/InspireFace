3117117e04b874dd3a50796e4448da833a36c6fb
JingyuYan <tunmxy@163.com>
2024-06-19 15:04:11 +0800

    fix symbol error bugs
    Former-commit-id: d5ffd8a94d5e61ec184eeeff95abfa3c4e7e7012

--- cpp/inspireface/middleware/inference_helper/inference_helper_rknn_adapter.cpp
+++ cpp/inspireface/middleware/inference_helper/inference_helper_rknn_adapter.cpp
@@ -153,7 +153,7 @@ std::vector<std::string> InferenceHelperRknnAdapter::GetInputNames() {
     return std::vector<std::string>();
 }
 
-int32_t InferenceHelperMnn::ResizeInput(const std::vector<InputTensorInfo>& input_tensor_info_list) {
+int32_t InferenceHelperRknnAdapter::ResizeInput(const std::vector<InputTensorInfo>& input_tensor_info_list) {
     // The function is not supported
     return 0;
 }
