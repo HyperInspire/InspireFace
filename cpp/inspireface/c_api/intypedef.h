8a5bb4b8fc9dc9921262f3ed216c8df79502bd50
tunm <tunmxy@163.com>
2024-11-18 15:01:28 +0800

    Added extended information api
    Former-commit-id: b1a472bcbbaf44f0a4285ce6bb0f4e33997813e4

--- cpp/inspireface/c_api/intypedef.h
+++ cpp/inspireface/c_api/intypedef.h
@@ -5,13 +5,14 @@
 #ifndef INSPIREFACE_INTYPEDEF_H
 #define INSPIREFACE_INTYPEDEF_H
 
+// clang-format off
 typedef void*               HPVoid;                           ///< Pointer to Void.
 typedef void*               HFImageStream;                   ///< Handle for image.
 typedef void*               HFSession;                       ///< Handle for context.
 typedef void*               HFImageBitmap;                   ///< Handle for image bitmap.
 typedef long                HLong;                            ///< Long integer.
-typedef float                HFloat;                          ///< Single-precision floating point.
-typedef float*               HPFloat;                         ///< Pointer to Single-precision floating point.
+typedef float               HFloat;                          ///< Single-precision floating point.
+typedef float*              HPFloat;                         ///< Pointer to Single-precision floating point.
 typedef double              HDouble;                          ///< Double-precision floating point.
 typedef	unsigned char		HUInt8;                           ///< Unsigned 8-bit integer.
 typedef signed int			HInt32;                           ///< Signed 32-bit integer.
@@ -21,9 +22,11 @@ typedef long                HResult;                          ///< Result code.
 typedef char*               HString;                          ///< String.
 typedef const char*         HPath;                            ///< Const String.
 typedef char                HBuffer;                            ///< Character.
+typedef char                HChar;                            ///< Character.
 typedef char*               HPBuffer;                           ///< Pointer Character.
 typedef long                HSize;                            ///< Size
 typedef long*               HPSize;                            ///< Pointer Size
+// clang-format on  
 
 typedef struct HFaceRect {
     HInt32 x;             ///< X-coordinate of the top-left corner of the rectangle.
