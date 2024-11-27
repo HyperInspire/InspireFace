fe01207353b0324b3bed84c70bcfafc3aaa92485
tunm <tunmxy@163.com>
2024-11-27 19:13:39 +0800

    Added more android api
    Former-commit-id: eff025f399025e2719ab10939f8e13efd2f726b7

---
+++ android/InspireFaceExample/inspireface/src/main/java/com/insightface/sdk/inspireface/base/Point2f.java
@@ -0,0 +1,6 @@
+package com.insightface.sdk.inspireface.base;
+
+public class Point2f {
+    public float x;
+    public float y;
+}
---
+++ android/InspireFaceExample/inspireface/src/main/java/com/insightface/sdk/inspireface/base/SearchTopKResults.java
@@ -0,0 +1,7 @@
+package com.insightface.sdk.inspireface.base;
+
+public class SearchTopKResults {
+    public int num;
+    public float[] confidence;
+    public int[] ids;
+}
