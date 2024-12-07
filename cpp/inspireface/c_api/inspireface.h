c6e20ceb0efb0d2f5b22c048e0344e877d881c76
Jingyu <tunmxy@163.com>
2024-12-07 17:53:35 +0800

    annotation
    Former-commit-id: 3dbe7c93347914b2d6ab1a875a25713924e39fa4

--- cpp/inspireface/c_api/inspireface.h
+++ cpp/inspireface/c_api/inspireface.h
@@ -878,10 +878,8 @@ typedef enum HFLogLevel {
     HF_LOG_DEBUG,     // Debug level for detailed system information mostly useful for developers
     HF_LOG_INFO,      // Information level for general system information about operational status
     HF_LOG_WARN,      // Warning level for non-critical issues that might need attention
-    HF_LOG_ERROR,     // Error level for error events that might still allow the application to
-                      // continue running
-    HF_LOG_FATAL      // Fatal level for severe error events that will presumably lead the application
-                      // to abort
+    HF_LOG_ERROR,     // Error level for error events that might still allow the application to continue running
+    HF_LOG_FATAL      // Fatal level for severe error events that will presumably lead the application to abort
 } HFLogLevel;
 
 /**
