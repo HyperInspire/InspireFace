eca2f57146220bb12656d75b639039028991e0dd
Jingyu <tunmxy@163.com>
2024-12-23 00:27:00 +0800

    Fix the build sc

--- .rknpu2_cache/MNN-2.3.0/benchmark/benchmark.h
+++
@@ -1,9 +0,0 @@
-//
-//  benchmark.h
-//  MNN
-//
-//  Created by MNN on 2021/04/13.
-//  Copyright © 2018, Alibaba Group Holding Limited
-//
-
-void iosBenchAll(const char* modelPath);
--- .rknpu2_cache/MNN-2.3.0/source/backend/vulkan/buffer/compiler/getSpirv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-glslangValidator -V $1 -Os -o __temp.spv
-spirv-dis __temp.spv > $2
-
-rm __temp.spv
