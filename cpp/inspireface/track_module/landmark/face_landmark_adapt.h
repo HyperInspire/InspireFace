fdc1938fba322b42e912c0caec30a89c55173aa9
tunm <tunmxy@163.com>
2025-02-20 15:08:44 +0800

    Replace the new face landmark model
    Former-commit-id: 5ce69db5fedac854f8e65c84fc1d00ac7c8eaafd

--- cpp/inspireface/track_module/landmark/face_landmark_adapt.h
+++ cpp/inspireface/track_module/landmark/face_landmark_adapt.h
@@ -38,13 +38,13 @@ public:
     int getInputSize() const;
 
 public:
-    const static int LEFT_EYE_CENTER = 55;     ///< Landmark index for the center of the left eye.
-    const static int RIGHT_EYE_CENTER = 105;   ///< Landmark index for the center of the right eye.
-    const static int NOSE_CORNER = 69;         ///< Landmark index for the tip of the nose.
-    const static int MOUTH_LEFT_CORNER = 45;   ///< Landmark index for the left corner of the mouth.
-    const static int MOUTH_RIGHT_CORNER = 50;  ///< Landmark index for the right corner of the mouth.
-    const static int MOUTH_LOWER = 37 - 1;
-    const static int MOUTH_UPPER = 104 - 1;
+    const static int LEFT_EYE_CENTER = 67;      ///< Landmark index for the center of the left eye.
+    const static int RIGHT_EYE_CENTER = 68;     ///< Landmark index for the center of the right eye.
+    const static int NOSE_CORNER = 100;         ///< Landmark index for the tip of the nose.
+    const static int MOUTH_LEFT_CORNER = 104;   ///< Landmark index for the left corner of the mouth.
+    const static int MOUTH_RIGHT_CORNER = 105;  ///< Landmark index for the right corner of the mouth.
+    const static int MOUTH_LOWER = 84;
+    const static int MOUTH_UPPER = 87;
 
     const static int NUM_OF_LANDMARK = 106;  ///< Total number of landmarks detected.
 
