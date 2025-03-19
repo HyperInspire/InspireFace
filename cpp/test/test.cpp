856e2b7fb13204a7baecfa6fbfe17e1b5a788165
Jingyu <tunmxy@163.com>
2025-03-19 20:19:52 +0800

    Update

--- cpp/test/test.cpp
+++ cpp/test/test.cpp
@@ -82,10 +82,10 @@ int main(int argc, char* argv[]) {
 
 #if defined(ISF_ENABLE_TENSORRT)
     HInt32 support_cuda;
-    ret = HFCheckCudaDeviceSupport(&support_cuda);
+    // ret = HFCheckCudaDeviceSupport(&support_cuda);
     // REQUIRE(ret == HSUCCEED);
     // REQUIRE(support_cuda == true);
-    // HFPrintCudaDeviceInfo();
+    HFPrintCudaDeviceInfo();
 #endif
 
     std::string fullPath;
