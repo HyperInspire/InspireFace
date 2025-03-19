404a5af97df6819db312c5fc9920a4c5d23330ca
tunm <tunmxy@163.com>
2025-03-19 18:54:19 +0800

    it

--- cpp/test/test.cpp
+++ cpp/test/test.cpp
@@ -84,7 +84,7 @@ int main(int argc, char* argv[]) {
     HInt32 support_cuda;
     ret = HFCheckCudaDeviceSupport(&support_cuda);
     REQUIRE(ret == HSUCCEED);
-    REQUIRE(support_cuda);
+    REQUIRE(support_cuda == true);
     HFPrintCudaDeviceInfo();
 #endif
 
