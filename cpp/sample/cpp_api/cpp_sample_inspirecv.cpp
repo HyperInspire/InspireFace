24910648d160a9e4a85920408ea50e50c3b2e249
Jingyu <tunmxy@163.com>
2025-04-15 22:53:23 +0800

    Update

--- cpp/sample/cpp_api/cpp_sample_inspirecv.cpp
+++ cpp/sample/cpp_api/cpp_sample_inspirecv.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
 #include <inspirecv/inspirecv.h>
+#include <inspireface/inspireface.hpp>
+#ifdef _WIN32
+#include <direct.h>
+#define CREATE_DIR(dir) _mkdir(dir)
+#else
+#include <sys/stat.h>
+#define CREATE_DIR(dir) mkdir(dir, 0777)
+#endif
 
 int main() {
+    // Make directory
+    if (CREATE_DIR("cv") == -1) {
+        // If the directory already exists, it is not an error
+        if (errno != EEXIST) {
+            std::cerr << "Error creating directory" << std::endl;
+            return 1;
+        }
+    }
+
     /* Image I/O */
 
     // Load image from file
-    // Load with 3 channels (RGB)
-    inspirecv::Image img = inspirecv::Image::Create("test_res/data/bulk/kun_cartoon_crop.jpg", 3);  
+    // Load with 3 channels (BGR, like opencv)
+    inspirecv::Image img = inspirecv::Image::Create("test_res/data/bulk/kun_cartoon_crop.jpg", 3);
 
     // Load image from buffer
     // uint8_t* buffer = ...;  // buffer is a pointer to the image data
-    // bool is_alloc_mem = false;  // if true, will allocate memory for the image data, 
+    // bool is_alloc_mem = false;  // if true, will allocate memory for the image data,
     //                             // false is recommended to point to the original data to avoid copying
     // inspirecv::Image img_buffer = inspirecv::Image::Create(width, height, channel, buffer, is_alloc_mem);
 
@@ -31,10 +48,11 @@ int main() {
     // Apply Gaussian blur
     inspirecv::Image blurred = img.GaussianBlur(3, 1.0);
     blurred.Write("cv/blurred.jpg");
-    
+
     // Geometric transformations
     auto scale = 0.35;
-    inspirecv::Image resized = img.Resize(img.Width() * scale, img.Height() * scale);  // Resize image
+    bool use_bilinear = true;
+    inspirecv::Image resized = img.Resize(img.Width() * scale, img.Height() * scale, use_bilinear);  // Resize image
     resized.Write("cv/resized.jpg");
 
     // Rotate 90 degrees clockwise
@@ -56,7 +74,7 @@ int main() {
 
     // Image padding
     int top = 50, bottom = 50, left = 50, right = 50;
-    inspirecv::Image padded = img.Pad(top, bottom, left, right, inspirecv::Color::Green);
+    inspirecv::Image padded = img.Pad(top, bottom, left, right, inspirecv::Color::Black);
     padded.Write("cv/padded.jpg");
 
     // Swap red and blue channels
@@ -69,7 +87,7 @@ int main() {
     scaled.Write("cv/scaled.jpg");
 
     // Add value to image
-    double value = 20;
+    double value = -175;
     inspirecv::Image added = img.Add(value);
     added.Write("cv/added.jpg");
 
@@ -82,7 +100,7 @@ int main() {
      * Create a transform matrix from the following matrix
      * [[a11, a12, tx],
      *  [a21, a22, ty]]
-     * 
+     *
      * Face crop transform matrix
      * [[0.0, -1.37626, 261.127],
      *  [1.37626, 0.0, 85.1831]]
@@ -94,12 +112,89 @@ int main() {
     float b1 = 261.127f;
     float b2 = 85.1831f;
 
-    inspirecv::TransformMatrix trans = inspirecv::TransformMatrix::Create(a11, a12, b1, 
-                                                                          a21, a22, b2 );
+    inspirecv::TransformMatrix trans = inspirecv::TransformMatrix::Create(a11, a12, b1, a21, a22, b2);
     int dst_width = 112;
     int dst_height = 112;
     inspirecv::Image affine = rotated_90.WarpAffine(trans, dst_width, dst_height);
     affine.Write("cv/affine.jpg");
-    
+
+    /* Image Draw */
+    inspirecv::Image draw_img = img.Clone();
+
+    // Draw a rectangle
+    inspirecv::Rect<int> new_rect = rect.Square(1.1f);  // Square and expand the rect
+    int thickness = 3;
+    draw_img.DrawRect(new_rect, inspirecv::Color::Green, thickness);
+    draw_img.Write("cv/draw_rect.jpg");
+
+    // Draw a circle
+    draw_img = img.Clone();
+    std::vector<inspirecv::Point<int>> points = new_rect.As<int>().ToFourVertices();
+    for (auto& point : points) {
+        draw_img.DrawCircle(point, 1, inspirecv::Color::Red, 5);
+    }
+    draw_img.Write("cv/draw_circle.jpg");
+
+    // Draw a line
+    draw_img = img.Clone();
+    draw_img.DrawLine(points[0], points[1], inspirecv::Color::Cyan, 2);
+    draw_img.DrawLine(points[1], points[2], inspirecv::Color::Magenta, 2);
+    draw_img.DrawLine(points[2], points[3], inspirecv::Color::Pink, 2);
+    draw_img.DrawLine(points[3], points[0], inspirecv::Color::Yellow, 2);
+    draw_img.Write("cv/draw_line.jpg");
+
+    // Fill a rectangle
+    draw_img = img.Clone();
+    draw_img.Fill(new_rect, inspirecv::Color::Purple);
+    draw_img.Write("cv/fill_rect.jpg");
+
+    // Reset
+    std::vector<uint8_t> gray_color(img.Width() * img.Height() * 3, 128);
+    img.Reset(img.Width(), img.Height(), 3, gray_color.data());
+    img.Write("cv/reset.jpg");
+
+    /** FrameProcess */
+
+    // BGR888 as raw data
+    inspirecv::Image raw = inspirecv::Image::Create("test_res/data/bulk/kun_cartoon_crop_r90.jpg", 3);
+    const uint8_t* buffer = raw.Data();
+
+    // You can also use other image format, like NV21, NV12, RGBA, RGB, BGR, BGRA
+    // const uint8_t* buffer = ...;
+
+    // Create frame process
+    auto width = raw.Width();
+    auto height = raw.Height();
+    auto rotation_mode = inspirecv::ROTATION_90;
+    auto data_format = inspirecv::BGR;
+    inspirecv::FrameProcess frame_process = inspirecv::FrameProcess::Create(buffer, height, width, data_format, rotation_mode);
+
+    // Set preview size
+    frame_process.SetPreviewSize(160);
+
+    // Set preview scale
+    // frame_process.SetPreviewScale(0.5f);
+
+    // Get transform image
+    inspirecv::Image transform_img = frame_process.ExecutePreviewImageProcessing(true);
+    transform_img.Write("cv/transform_img.jpg");
+
+    // ExecuteImageAffineProcessing
+
+    // Face crop transform matrix
+    // [[0.0, 0.726607, -61.8946],
+    //  [-0.726607, 0.0, 189.737]]
+    a11 = 0.0f;
+    a12 = 0.726607f;
+    a21 = -0.726607;
+    a22 = 0.0f;
+    b1 = -61.8946f;
+    b2 = 189.737f;
+    inspirecv::TransformMatrix affine_matrix = inspirecv::TransformMatrix::Create(a11, a12, b1, a21, a22, b2);
+    dst_width = 112;
+    dst_height = 112;
+    inspirecv::Image affine_img = frame_process.ExecuteImageAffineProcessing(affine_matrix, dst_width, dst_height);
+    affine_img.Write("cv/affine_img.jpg");
+
     return 0;
 }
